2750374b1306089e8fdfc21be57237842d980d60
Ilya <sange0337@gmail.com>
2025-02-11 16:36:10 +0800

    create a separate component for input field

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,12 +1,18 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import {
+    forwardRef,
+    useEffect,
+    useLayoutEffect,
+    useRef,
+    useState
+} from 'react';
 
 export function AddExpenseForm({ onClose, isOpen, setExpenses }) {
     const [isClosing, setIsClosing] = useState(false);
     const [showMessage, setShowMessage] = useState(false);
-    const prevIsOpen = useRef();
-    const amountRef = useRef();
-    const dateRef = useRef();
-    const descriptionRef = useRef();
+    const prevIsOpen = useRef(null);
+    const amountRef = useRef(null);
+    const dateRef = useRef(null);
+    const descriptionRef = useRef(null);
 
     useEffect(() => {
         function handler(e) {
@@ -32,7 +38,7 @@ export function AddExpenseForm({ onClose, isOpen, setExpenses }) {
 
     function addExpense(e) {
         e.preventDefault();
-        console.log(dateRef.current.value)
+        console.log(dateRef.current.value);
         const date = dateRef.current.value;
         const description = descriptionRef.current.value;
         const amount = amountRef.current.value;
@@ -49,7 +55,7 @@ export function AddExpenseForm({ onClose, isOpen, setExpenses }) {
 
         setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
         setShowMessage(true);
-        setTimeout(() => setShowMessage(false), 4000)
+        setTimeout(() => setShowMessage(false), 4000);
     }
 
     return (
@@ -63,65 +69,31 @@ export function AddExpenseForm({ onClose, isOpen, setExpenses }) {
             <div className="overlay w-full h-full">
                 <div
                     onClick={(e) => e.stopPropagation()}
-                    className="modal-body ml-auto bg-light-gray max-w-80 h-full p-6 space-y-4"
+                    className="modal-body ml-auto bg-light-gray max-w-80 h-full p-6 space-y-4 overflow-y-auto"
                 >
                     <header className="flex items-center justify-between gap-2">
                         <div className="text-3xl font-light">Add</div>
-                        {showMessage && <div className='toast'>Added!</div>}
-                        <button onClick={onClose} className="text-4xl font-thin cursor-pointer">
+                        {showMessage && <div className="toast">Added!</div>}
+                        <button
+                            onClick={onClose}
+                            className="text-4xl font-thin cursor-pointer"
+                        >
                             &times;
                         </button>
                     </header>
                     <form onSubmit={(e) => addExpense(e)} className="space-y-4">
                         <div className="bg-white space-y-4 py-4 px-2">
-                            <div>
-                                <label
-                                    htmlFor="amount"
-                                    className="titles text-xs mb-2"
-                                >
-                                    Expense ($)
-                                </label>
-                                <input
-                                    type="number"
-                                    id="amount"
-                                    className="border-b border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    placeholder="0"
-                                    required
-                                    ref={amountRef}
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="date"
-                                    className="titles text-xs mb-2"
-                                >
-                                    Date
-                                </label>
-                                <input
-                                    type="date"
-                                    id="date"
-                                    className="border-b border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    placeholder="0"
-                                    required
-                                    ref={dateRef}
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="description"
-                                    className="titles text-xs mb-2"
-                                >
-                                    description
-                                </label>
-                                <input
-                                    type="text"
-                                    id="description"
-                                    className="border-b border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder:text-gray-400"
-                                    placeholder="For example, groceries"
-                                    required
-                                    ref={descriptionRef}
-                                />
-                            </div>
+                            <FormField
+                                ref={amountRef}
+                                type="number"
+                                text="Expense ($)"
+                            />
+                            <FormField ref={dateRef} type="date" text="Date" />
+                            <FormField
+                                ref={descriptionRef}
+                                type="text"
+                                text="description"
+                            />
                         </div>
                         <button className="btn-primary w-full py-4 mt-6">
                             Add new
@@ -132,3 +104,21 @@ export function AddExpenseForm({ onClose, isOpen, setExpenses }) {
         </div>
     );
 }
+
+const FormField = forwardRef(function FormFieldRef({ type, text }, ref) {
+    return (
+        <div>
+            <label htmlFor="amount" className="titles text-xs mb-2">
+                {text}
+            </label>
+            <input
+                type={type}
+                id="amount"
+                className="border-b border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="0"
+                required
+                ref={ref}
+            />
+        </div>
+    );
+});
